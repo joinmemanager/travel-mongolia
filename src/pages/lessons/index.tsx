b63ceeea53ed15d133346b6e85d1d5f26ec4a7de
dashka0212 <82076675+cody-dashka@users.noreply.github.com>
2022-05-02 21:43:16 +0800

    implement styled component

--- src/pages/lessons/index.tsx
+++ src/pages/lessons/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 import { Meta } from '@/layout/Meta';
 import { Main } from '@/templates/Main';
