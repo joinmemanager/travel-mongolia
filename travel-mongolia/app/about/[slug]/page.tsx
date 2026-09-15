15835bd893cf0be8907f248416711bae8a0e0f96
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2026-09-15 12:07:21 +0800

    fix AboutShowcase import path

--- travel-mongolia/app/about/[slug]/page.tsx
+++ travel-mongolia/app/about/[slug]/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic';
 
 import React from 'react';
-import AboutShowcase from '../../components/AboutShowcase';
+import AboutShowcase from '../../../components/AboutShowcase';
 
 interface Props {
   params: Promise<{ slug: string }>;
