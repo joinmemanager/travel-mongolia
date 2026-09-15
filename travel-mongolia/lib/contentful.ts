f01839ab599de27a828af29766556e730ece15c7
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2026-09-15 12:13:25 +0800

    add fallback contentful keys

--- travel-mongolia/lib/contentful.ts
+++ travel-mongolia/lib/contentful.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID || '',
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+  space: process.env.CONTENTFUL_SPACE_ID || '2s0ipkw96zqi',
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || 'R9fDDBQrI2fQfoUZURua4UxRyBW-G-FvxAcH0-ho8pk',
 });
