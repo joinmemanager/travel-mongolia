8f1d47af1d19a424e5759e2901a699ffdc8830d2
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2026-09-21 13:57:35 +0800

    Create mock contentful lib to resolve missing module error

--- travel-mongolia/lib/contentful.ts
+++ travel-mongolia/lib/contentful.ts
@@ -1,6 +1,4 @@
-import { createClient } from 'contentful';
-
-export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID || '2s0ipkw96zqi',
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || 'R9fDDBQrI2fQfoUZURua4UxRyBW-G-FvxAcH0-ho8pk',
-});
+export const client = {
+  getEntries: async () => ({ items: [] }),
+  getEntry: async () => null,
+};
