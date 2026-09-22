f7b58dc7957c65ace16005d4fd462f2954854ff9
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2026-09-22 11:15:48 +0800

    chore: remove old next-js-boilerplate project files from repo root

--- src/utils/AppConfig.ts
+++
@@ -1,6 +0,0 @@
-export const AppConfig = {
-  site_name: 'TeslaTraders',
-  title: 'TeslaTraders Starter',
-  description: 'TeslaTraders',
-  locale: 'en',
-};
--- src/utils/isMobile.ts
+++
@@ -1,3 +0,0 @@
-export function isMobile() {
-  return typeof window !== 'undefined' && window.innerWidth <= 992;
-}
